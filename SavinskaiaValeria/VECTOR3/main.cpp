c444c3a853a03545dc661b5eb69916b9fa5ddba3
ValerSavin <75761026+ValerSavin@users.noreply.github.com>
2020-12-10 21:24:35 +0300

    Create main.cpp

---
+++ SavinskaiaValeria/VECTOR3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "std_lib_facilities.h"
+#include "aaaaaaaaa.h"
+
+using namespace std;
+
+int main()
+{
+    vectop<int> vec = {1,2,3,4,5,6,7,8,9,10};
+    vectop<string> vec_ = {"a", "aa", "aaa"};
+    
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << ' ';
+    cout << '\n';
+
+    for(int i = 0; i < vec_.size(); i++)
+        cout << vec_[i] << ' ';
+    cout << '\n';
+}
